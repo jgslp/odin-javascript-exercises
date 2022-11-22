fb299a48635aa4716bf319196dda9fc5e9fe0de8
Jacqueline Gellick <jgellick@gmail.com>
2022-11-22 21:55:56 +0100

    Completed fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(digit) {
+    if (digit < 0) {
+        return 'OOPS';
+    }
+    let array = [1, 1];
+    for (let i = 2; i < digit; i++) {
+        array.push(array[i - 1] + array[i - 2]);
+    }
+    return array[digit - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
