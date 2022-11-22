b98973ed8f66e3be3d716dca76c291d498485648
Jacqueline Gellick <jgellick@gmail.com>
2022-11-22 21:28:13 +0100

    Completed sum function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(x, y) {
 	 return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((acc, item) => acc + item, 0)
 };
 
 const multiply = function() {
