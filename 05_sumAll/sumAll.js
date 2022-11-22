9e57d22f8f06c5c674e7bdeba14246cfa6f02ab5
Jacqueline Gellick <jgellick@gmail.com>
2022-11-22 11:26:37 +0100

    Completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    const array = [];
+    if (typeof x !== "number" || typeof y !== "number") {
+        return "ERROR";
+    }
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    } 
+    if (x < y) {
+        for (let i = x; i <= y; i++) {
+            array.push(i);
+        }
+    } else if (y < x) {
+        for (let i = y; i <= x; i++) {
+            array.push(i);
+        }
+    }
+    return array.reduce((acc, item) => acc + item, 0)
 };
 
 // Do not edit below this line
