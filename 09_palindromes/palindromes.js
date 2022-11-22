da1095097d61f91d0160ecd7f6f57d83e4b2e832
Jacqueline Gellick <jgellick@gmail.com>
2022-11-22 21:44:03 +0100

    Completed palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reducedString = string.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    let reversedString = reducedString.split("").reverse().join("");
+    return (reducedString === reversedString)
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
